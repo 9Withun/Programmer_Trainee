92a11a4f51f38325b56216759306af2984d824fa
withun <ptaworsin@gmail.com>
2026-02-10 23:25:29 +0700

    newcommit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -37,7 +37,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+<<<<<<< Updated upstream
 //สวดยอดเลย
+=======
+@Composable
+fun Input(Text : String){
+
+}
+
+>>>>>>> Stashed changes
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
